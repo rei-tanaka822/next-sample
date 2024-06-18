21f249b3bbe57ba29e3fa338328657ba60c25f69
r_tanaka <r_tanaka@active.co.jp>
2024-06-19 08:45:31 +0900

    設定ファイル更新

---
+++ src/lib/db.js
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+});
+
+export const query = (text, params) => pool.query(text, params);
