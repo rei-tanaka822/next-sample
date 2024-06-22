87e261256782eeefff6824beb39f2ca0ba4df359
r_tanaka <r_tanaka@active.co.jp>
2024-06-22 18:57:38 +0900

    インデント修正

--- src/pages/api/contactList.ts
+++ src/pages/api/contactList.ts
@@ -2,24 +2,24 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { query } from '../../lib/db';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-try {
-    const sql =
-        ' SELECT' +
-        '   number,' +
-        '   subject,' +
-        '   client_name,' +
-        '   status_name AS status,' +
-        '   user_name AS person_in_charge' +
-        ' FROM contact c' +
-        ' LEFT JOIN status_mst sm' +
-        '   ON c.status = sm.id' +
-        ' LEFT JOIN user_mst um' +
-        '   ON c.person_in_charge = um.id';
+  try {
+      const sql =
+          ' SELECT' +
+          '   number,' +
+          '   subject,' +
+          '   client_name,' +
+          '   status_name AS status,' +
+          '   user_name AS person_in_charge' +
+          ' FROM contact c' +
+          ' LEFT JOIN status_mst sm' +
+          '   ON c.status = sm.id' +
+          ' LEFT JOIN user_mst um' +
+          '   ON c.person_in_charge = um.id';
 
-    const result = await query(sql);
-    res.status(200).json(result.rows);
-} catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-}
+      const result = await query(sql);
+      res.status(200).json(result.rows);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
